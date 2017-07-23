2d1e91a42b772d56f72c7001c2177ea884a528cd
jeremy <jeremybowyer@gmail.com>
2017-07-23 14:05:41 -0400

    Adding test comment to FunFacts.java

--- app/src/main/java/com/jeremybowyer/funfacts/FunFacts.java
+++ app/src/main/java/com/jeremybowyer/funfacts/FunFacts.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class FunFacts extends AppCompatActivity {
-
+    // This is a test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
